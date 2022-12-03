f9f08f7efcf80173dbdab324a5ced71d0b601a0f
Alex <alpha_sfairat@mail.ru>
2022-12-03 18:51:59 +0300

    added Homework_4

---
+++ Homework_4/Task 25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int sum = 1;
+for (int i = 1; i <= B; i++)
+{
+    sum *= A;
+}
+
+Console.WriteLine($"Первое введенное число в степени второго равно: {sum}");
---
+++ Homework_4/Task 29/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while(index < lenght)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+Console.WriteLine("Введите число элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[a];
+
+FillArray(array);
+PrintArray(array);
