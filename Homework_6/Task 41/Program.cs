e9260c09a09749f27727893f14bc1cc609154618
Alex <alpha_sfairat@mail.ru>
2022-12-10 19:24:58 +0300

    added Homework_6

---
+++ Homework_6/Task 41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите любое количество чисел в строку: ");
+string numberM = Console.ReadLine() ?? String.Empty;
+string[] tempArray = numberM.Split(new char[]{' ', '.', ','}, StringSplitOptions.RemoveEmptyEntries);
+int[] arrayM = tempArray.Select(Int32.Parse).ToArray();
+
+int i = 0;
+foreach (var num in arrayM)
+{
+    if (num > 0)
+    {
+        i++;
+    }
+}
+Console.WriteLine($"Введено чисел больше нуля: {i}.");
---
+++ Homework_6/Task 43/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите число b1: ");
+float b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k1: ");
+float k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b2: ");
+float b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k2: ");
+float k2 = Convert.ToInt32(Console.ReadLine());
+
+float x = (b2 - b1) / (k1 - k2);
+float y = k1 * x + b1;
+
+Console.WriteLine($"Координаты точки пересечения заданных прямых: x = {x}, y = {y}.");
