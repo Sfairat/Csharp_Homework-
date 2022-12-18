24092a306d73761a22f0a9c116c08ecbe0f771c8
Alex <alpha_sfairat@mail.ru>
2022-12-18 16:51:36 +0300

    added Homework_9

---
+++ Homework_9/Task 66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Nums(int a, int b)
+{
+    if(a == b) return b;
+    else return a + Nums(a + 1, b);
+}
+
+Console.WriteLine($"Сумма элементов между числами M и N равна: {Nums(M, N)}.");
---
+++ Homework_9/Task 68/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Ackerman(int a, int b)
+{
+    if(a == 0)
+    {
+        return b + 1;
+    }
+    else if(b == 0)
+    {
+        return Ackerman(a - 1, 1);
+    }
+    else
+    {
+        return Ackerman(a - 1, Ackerman(a, b - 1));
+    }
+}
+
+if(m < 0 || n < 0)
+{
+    Console.WriteLine("Задано отрицательное число. Вычисление невозможно!");
+}
+else
+{
+    Console.WriteLine($"Значение функции Аккермана для чисел {m} и {n} будет равно {Ackerman(m, n)}.");
+}
